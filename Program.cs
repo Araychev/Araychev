f2320fb67cb95266a3e0d65f32490bf6b4738d4d
Angel Raychev <77881158+Araychev@users.noreply.github.com>
2021-03-22 20:22:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pound = decimal.Parse(Console.ReadLine());
+
+            decimal dolars = pound*1.31m;
+            Console.WriteLine($"{dolars:f3}");
+        }
+    }
+}
